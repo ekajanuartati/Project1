5337f88f249898a34adc26943440d7d77eef46f7
ekajanuartati <ekajanuartati@gmail.com>
2024-02-09 21:59:50 +0800

    add this

---
+++ Hello.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+
+df = pd.read_csv(r"C:/Users/HP/Downloads/automobile/imports-85.data") 
+print(df)
---
+++ basecode.py
@@ -0,0 +1,35 @@
+print('First Program - Python print function')
+print('It is declared like this:')
+print("print('want to print')")
+
+#String Manipulation
+print("hello word\nhello word")
+print("hellow word \n hello word")
+print("name eka\nname kadek")
+print("eka" + "januartati")
+print("eka"+ " "+"januartai")
+
+print('srting concatenation is done with"+"sign\nalso i wrote "X" here for whatever reason')
+print("welcome" , "to" , "gr")
+print("welcome", end=" ")
+print("to Gr")
+
+price=100
+tax=18
+total_price= price + tax
+print(total_price)
+
+x=10
+y="geeks"
+z=20
+w=30
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+print('x')
+
+name = input("enter the name=")
+Print("welcome", name)
